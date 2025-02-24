312a7b14d67535dd27e0a809d18634bcc88e7221
Sidney-Land <136678741+Sidney-Land@users.noreply.github.com>
2025-02-24 13:13:36 -0500

    Added FoodSpawner.cs script and a Scripts folder
    + FoodSpawner.cs, which should be attached to the game manager, spawns a random food from a given array of prefabs within a set boundary every foodDelay seconds.
    
    * I will now begin testing this script and building the food prefabs.

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    // Time before each food spawn
+    public float foodDelayMax;
+    // Current time left before next food spawn
+    private float foodDelay;
+
+    // Furthest food can spawn on the x-axis
+    public float xBound;
+    // Furthest food can spawn on the z-axis
+    public float zBound;
+
+    // Array of food prefabs to spawn
+    public GameObject[] foodPrefabs;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        foodDelay = foodDelayMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Countdown foodDelay
+        foodDelay -= Time.deltaTime;
+
+        // Spawn food when foodDelay seconds elapses, reset timer
+        if (foodDelay <= 0.0f)
+        {
+            SpawnFood();
+
+            foodDelay = foodDelayMax;
+        }
+    }
+
+    // Spawns a random food at random location (within the bounds)
+    void SpawnFood()
+    {
+        // Choose random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-xBound, xBound), 0f, Random.Range(-zBound, zBound));
+
+        // Spawn random food at spawnPos
+        Instantiate(foodPrefabs[Random.Range(0, foodPrefabs.Length - 1)], spawnPos, Quaternion.identity);
+    }
+}
